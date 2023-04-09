6b554500b6428c25f3a333bc461db50f5cf0f9ba
Victor Caro <victor.caro.rainao@gmail.com>
2023-04-09 16:38:21 -0400

    models

--- src/main/java/com/femit/femit/models/Portafolio.java
+++ src/main/java/com/femit/femit/models/Portafolio.java
@@ -1,9 +1,12 @@
 package com.femit.femit.models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,4 +23,19 @@ public class Portafolio {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_portafolio;
     private Integer id_usuario;
+    
+    @OneToMany(mappedBy = "portafolio")
+    private List<Proyecto> proyectos;
+    
+    
+    // Métodos adicionales
+    public void addProyecto(Proyecto proyecto) {
+        proyectos.add(proyecto);
+        proyecto.setId_portafolio(id_portafolio);
+    }
+    
+    public void removeProyecto(Proyecto proyecto) {
+        proyectos.remove(proyecto);
+        proyecto.setId_portafolio(null);
+    }
 }
